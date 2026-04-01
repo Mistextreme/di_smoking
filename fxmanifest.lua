1b26e3d0c8dfc0eb1e60b821f776d4f2d647ce7e
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2026-04-01 10:59:49 +0100

    Upload: fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,32 @@
+fx_version 'cerulean'
+lua54 'yes'
+game 'gta5'
+
+author 'DOTINIT SCRIPTS'
+description 'Advanced Cigarette & Smoking System'
+version '1.1.0'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'shared/config.lua'
+}
+
+client_scripts {
+    'client/client.lua'
+}
+
+server_scripts {
+    'server/server.lua',
+    'server/webhook.lua'
+
+}
+
+dependencies {
+    'ox_lib'
+}
+
+escrow_ignore {
+    'shared/config.lua',
+    'server/webhook.lua'
+}
+dependency '/assetpacks'
